ab20d930a765b7cc98df43a2342994b708bc5b87
Takatoshi Wada <fueledbytaching@gmail.com>
2021-07-23 07:35:14 +0800

    feat: validation, update tasks, filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,8 @@
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -7,12 +10,35 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if(Object.keys(filterDto).length){
+      return this.tasksService.getTasksWithFilter(filterDto);
+    } else {
     return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id)
   }
 
   @Post()
-  createTask(@Body('title') title: string, @Body('description') description: string) : Task {
-    return this.tasksService.createTask(title, description)
+  createTask(@Body() createTaskDto: CreateTaskDto) : Task {
+    return this.tasksService.createTask(createTaskDto)
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id:string): void {
+    return  this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id:string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+    ): Task {
+    const { status } = updateTaskStatusDto
+    return this.tasksService.updataTaskStatus(id, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task, TaskStatus } from './task.model';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
@@ -8,7 +10,36 @@ export class TasksService {
     getAllTasks(): Task[] {
         return this.tasks;
     }
-    createTask(title: string, description: string): Task {
+
+    getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+        const {status, search} = filterDto;
+
+        let tasks = this.getAllTasks();
+        if(status) {
+            tasks = tasks.filter((task) => task.status === status);
+        }
+        if(search) {
+            tasks = tasks.filter((task) => {
+                if(task.title.includes(search) || task.description.includes((search))) {
+                    return true;
+                }
+                return false;
+            })
+        }
+        return tasks;
+    }
+
+    getTaskById(id: string): Task {
+
+        const found =  this.tasks.find((task) => task.id === id);
+        if(!found) {
+            throw new NotFoundException();
+        }
+        return found;
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const {title, description} = createTaskDto
         const task: Task = {
             id: uuid(),
             title,
@@ -18,5 +49,15 @@ export class TasksService {
 
         this.tasks.push(task);
         return task
+    }
+    deleteTask(id: string): void {
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter((task) => task.id !== found.id);
+    }
+
+    updataTaskStatus(id: string, status: TaskStatus) {
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
     }
 }
